7e266c1ce428fa4c6d3c0e84e491c8fc379ba036
72681474+LakhindarPal@users.noreply.github.com <72681474+LakhindarPal@users.noreply.github.com>
2024-01-11 15:59:04 +0530

    new UI

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# link-tree
+# Lakhindar Pal - Links
 Explore my link tree for quick access to my social profiles.
 
-Check here https://lakhindarpal.pages.dev/
+[Check here](https://lakhindarpal.pages.dev)
--- pwa/sw.js
+++ pwa/sw.js
@@ -1,13 +1,16 @@
-// Specify what we want added to the cache for offline use
+// cache for offline use
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("lp-links-cache").then(cache => {
+    caches.open("lp-cache-v2.0.0").then(cache => {
       return cache.addAll([
         "/",
         "/index.html",
-        "/style.css",
+        "/success.html",
+        "/styles.css",
+        "/main.js",
         "/logos",
-        "/pics",
+        "/icons",
+        "/pics"
       ]);
     })
   );
